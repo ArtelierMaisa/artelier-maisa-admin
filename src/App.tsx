f5a6d5e7bc0a05d8ce732c018c76d62a120f3c3f
Josias Pereira <josias.ngpereira@gmail.com>
2024-05-13 23:06:20 -0300

    feat: add phosphor-icons/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Horse, Heart, Cube } from "@phosphor-icons/react";
+
 function App() {
 
   return (
-    <h1 className="text-1xl font-bold underline">Josias fodão</h1>
+    //<h1 className="text-1xl font-bold underline">Josias fodão</h1>
+    <main>
+      <Horse />
+      <Heart color="#AE2983" weight="fill" size={32} />
+      <Cube color="teal" weight="duotone" />
+    </main>
   )
-  
+
 }
 
 export default App
