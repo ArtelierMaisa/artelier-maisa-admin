ece466318428b37d2c85eac33c325b965ad64c7c
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-19 17:37:26 -0300

    feat: add handle user sign-in with Firebase

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,11 @@
 import { createContext, PropsWithChildren, useEffect, useState } from 'react';
 
-import { AuthContextProps, AuthStorageProps, UserSignInProps } from '../@types';
+import { AuthContextProps, AuthStorageProps, User, UserSignInProps } from '../@types';
 import { AsyncStorage } from '../storage';
+import { ref, get } from 'firebase/database';
+import { database } from '../services';
+import { toast } from 'sonner';
+import { authenticate } from '../helpers/auth';
 
 export const AuthContext = createContext({} as AuthContextProps);
 
@@ -19,8 +23,33 @@ export function AuthProvider({ children }: Required<PropsWithChildren>) {
   }
 
   // TODO: Add login with Firebase here!
-  function handleSignIn(user: UserSignInProps): void {
+  async function handleSignIn(user: UserSignInProps): Promise<boolean> {
     const currentDate = new Date();
+
+    const loginRef = ref(database, 'user');
+    const userSnapshot = await get(loginRef);
+
+    if (!userSnapshot.exists()) {
+      toast.error('Usuário não encontrado! Desculpe, não conseguimos encontrar uma conta com esse nome de usuário. Por favor, verifique suas informações e tente novamente.', {
+        duration: 7500,
+      });
+
+      return false;
+    }
+
+    const userFirebase: User = Object.keys(userSnapshot.val()).map(key => ({
+      ...userSnapshot.val(),
+    }))[0];
+
+    const isUserAuthenticated = await authenticate(user, userFirebase);
+
+    if (!isUserAuthenticated || !userFirebase) {
+      toast.error('Falha no Login! Algo deu errado durante o login. Por favor, tente novamente. Se o problema persistir, entre em contato com o suporte técnico.', {
+        duration: 7500,
+      });
+
+      return false;
+    }
 
     const userToStorage: AuthStorageProps = {
       isAuthenticated: true,
@@ -29,6 +58,8 @@ export function AuthProvider({ children }: Required<PropsWithChildren>) {
 
     AsyncStorage.set<AuthStorageProps>('auth', userToStorage);
     setIsAuthenticated(true);
+
+    return true;
   }
 
   function handleSignOut(): void {
