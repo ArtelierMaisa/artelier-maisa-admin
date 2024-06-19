2ac301f0f9c1eabf3c843936a1795f2d3e0d5c6e
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-06-19 13:51:35 -0300

    style: add scroll Y auto for `Details` and small fixes in styles and structure

--- src/components/ProductModal/Photos/index.tsx
+++ src/components/ProductModal/Photos/index.tsx
@@ -24,7 +24,7 @@ export function Photos(props: PhotosProps) {
       onSubmit={handleAdd}
       className='relative flex flex-col w-full md:w-[40rem] h-full md:h-auto md:max-h-[90%] items-center overflow-hidden rounded-none md:rounded-2xl px-6 md:px-10 py-6 gap-2 bg-white shadow-default scrollbar scrollbar-w-3 scrollbar-thumb-rounded-lg scrollbar-thumb-primary scrollbar-track-white-color overflow-y-scroll'
     >
-      <div className='m-auto'>
+      <div className='w-full m-auto'>
         <button
           type='button'
           className='absolute flex top-0 right-0 justify-center items-center w-8 h-8 bg-primary rounded-none cursor-pointer hover:opacity-90 transition-colors duration-300'
@@ -61,28 +61,20 @@ export function Photos(props: PhotosProps) {
         </div>
 
         <div className='hidden sm:flex flex-col w-full justify-center md:justify-start items-center gap-2 px-4'>
-          <div className='flex flex-row w-full justify-center md:justify-start items-center gap-1'>
-            <Icon variant='info' color='primary' />
+          <Text className='flex flex-row w-full justify-center md:justify-start items-center gap-1'>
+            <Icon variant='info' color='primary' /> Clique em{' '}
+            <Icon variant='pencil' color='primary' /> para editar a foto do
+            produto.
+          </Text>
 
-            <Text>Clique em</Text>
-
-            <Icon variant='pencil' color='primary' />
-
-            <Text>para editar a foto do produto.</Text>
-          </div>
-
-          <div className='flex flex-row w-full justify-center md:justify-start items-center gap-1'>
-            <Icon variant='info' color='primary' />
-
-            <Text>Clique em</Text>
-
-            <Icon variant='trash' color='primary' />
-
-            <Text>para excluir a foto do produto.</Text>
-          </div>
+          <Text className='flex flex-row w-full justify-center md:justify-start items-center gap-1'>
+            <Icon variant='info' color='primary' /> Clique em{' '}
+            <Icon variant='trash' color='primary' /> para excluir a foto do
+            produto.
+          </Text>
         </div>
 
-        <div className='flex flex-col sm:flex-row w-full mt-6 px-4 gap-2 justify-center items-center'>
+        <div className='flex flex-col sm:flex-row w-full mt-6 sm:px-4 gap-2 justify-center items-center'>
           <GenericButton
             variant='secondary'
             type='medium'
