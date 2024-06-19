5fca283a9791f5e4586a6d6dbff3f264b0be8a33
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-19 17:36:41 -0300

    feat: add authentication helper function

---
+++ src/helpers/auth.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcryptjs';
+import { User, UserSignInProps } from '../@types';
+
+export async function authenticate(user: UserSignInProps, userFirebase: User) {
+  const isEmailValid = await bcrypt.compare(user.email, userFirebase.email);
+  const isPasswordValid = await bcrypt.compare(user.password, userFirebase.password);
+
+  return isEmailValid && isPasswordValid;
+}
