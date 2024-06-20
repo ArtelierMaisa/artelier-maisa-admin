86d258cb36da059b2ab725aac95a7f19ff19eaa3
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-20 15:33:46 -0300

    feat: add `highlights` property and `handleGetHighlights` function to `UserContextProps`

--- src/@types/contexts/UserContext.ts
+++ src/@types/contexts/UserContext.ts
@@ -1,6 +1,8 @@
-import { About } from "../services";
+import { About, Highlight } from "../services";
 
 export interface UserContextProps {
   about: About;
+  highlights: Highlight[];
+  handleGetHighlights(): Promise<void>;
   handleGetAbout(): Promise<void>;
 }
