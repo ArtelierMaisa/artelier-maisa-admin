2fe6879be65cb4ef0fc6cffb0d77f53cfc378c41
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-20 15:34:32 -0300

    feat: add `handleGetHighlights` function to `UserProvider`

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,9 +1,8 @@
 import { createContext, PropsWithChildren, useState } from 'react';
-
-import { About, UserContextProps } from '../@types';
-import { ref, get } from 'firebase/database';
-import { database } from '../services';
 import { ExternalToast, toast } from 'sonner';
+import { ref, get, remove } from 'firebase/database';
+import { About, Highlight, UserContextProps } from '../@types';
+import { database } from '../services';
 
 // TODO: Create type to UserContext in @types/contexts.
 export const UserContext = createContext({} as UserContextProps);
@@ -12,6 +11,7 @@ export const UserContext = createContext({} as UserContextProps);
 
 export function UserProvider({ children }: Required<PropsWithChildren>) {
   const [about, setAbout] = useState({} as About);
+  const [highlights, setHighlights] = useState([] as Highlight[]);
   const toastOptions: ExternalToast = { duration: 7500 };
 
 
@@ -22,12 +22,16 @@ export function UserProvider({ children }: Required<PropsWithChildren>) {
     );
   }
 
+  function isGreaterThanPeriodRemove(highlight: Highlight): boolean {
+    const currentTime = new Date().getTime();
+    return currentTime > highlight.removedAt;
+  }
+
   async function handleGetAbout(): Promise<void> {
     const aboutRef = ref(database, 'about');
     const aboutSnapshot = await get(aboutRef);
 
     if (!aboutSnapshot.exists()) return handleGenericErrorToast();
-
 
     const aboutFirebase: About = Object.keys(aboutSnapshot.val()).map(key => ({
       ...aboutSnapshot.val()[key],
@@ -36,11 +40,36 @@ export function UserProvider({ children }: Required<PropsWithChildren>) {
 
     if (!aboutFirebase) return handleGenericErrorToast();
 
-
     setAbout(aboutFirebase);
   }
 
-  return <UserContext.Provider value={{ handleGetAbout, about }}>{children}</UserContext.Provider>;
+  async function handleGetHighlights(): Promise<void> {
+    const highlightsRef = ref(database, 'highlights');
+    const highlightsSnapshot = await get(highlightsRef);
+
+    if (!highlightsSnapshot.exists()) return setHighlights([]);
+
+    const highlightsFirebase: Highlight[] = Object.keys(highlightsSnapshot.val()).map(key => ({
+      ...highlightsSnapshot.val()[key],
+      id: key,
+    }));
+
+    if (!highlightsFirebase) return handleGenericErrorToast();
+
+    // TODO -> REMOVE HIGHLIGHTS IMAGES FROM FIREBASE STORAGE
+    highlightsFirebase.forEach(async (hightlight) => {
+      if (isGreaterThanPeriodRemove(hightlight)) {
+        const highlightRef = ref(database, `highlights/${hightlight.id}`);
+        remove(highlightRef);
+      }
+    })
+
+    const highlightsInPeriod = highlightsFirebase.filter(highlight => !isGreaterThanPeriodRemove(highlight));
+
+    setHighlights(highlightsInPeriod);
+  }
+
+  return <UserContext.Provider value={{ handleGetAbout, handleGetHighlights, about, highlights }}>{children}</UserContext.Provider>;
 }
 
 
