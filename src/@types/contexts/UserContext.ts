e25796b152a72769c20fd3051dd2e2323f85b2bc
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-20 16:25:43 -0300

    feat: add `banners` property and `handleGetBanners` function to `UserContextProps`

--- src/@types/contexts/UserContext.ts
+++ src/@types/contexts/UserContext.ts
@@ -1,8 +1,10 @@
-import { About, Highlight } from "../services";
+import { About, Banner, Highlight } from "../services";
 
 export interface UserContextProps {
   about: About;
+  banners: Banner[];
   highlights: Highlight[];
   handleGetHighlights(): Promise<void>;
+  handleGetBanners(): Promise<void>;
   handleGetAbout(): Promise<void>;
 }
