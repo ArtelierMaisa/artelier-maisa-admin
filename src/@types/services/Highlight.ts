19be7fc1c3f9f7c70653c606d44509f598e162c2
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-20 16:25:23 -0300

    refactor: update `Banner` and `Highlight` interfaces with `image` property

--- src/@types/services/Highlight.ts
+++ src/@types/services/Highlight.ts
@@ -1,8 +1,14 @@
+interface ImageProps {
+  id: string;
+  name: string;
+  uri: string;
+}
+
 export interface Highlight {
   id: string;
   title: string;
   description: string;
-  uri: string;
+  image: ImageProps;
   createdAt: number;
   removedAt: number;
 }
