ad0731c95b3b078a23d48af0c27384e16bbac4e7
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-06-23 19:58:16 -0300

    feat: integrate about information in `About` page

--- src/pages/admin/About.tsx
+++ src/pages/admin/About.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, FormEvent, useRef, useState } from 'react';
-import { toast } from 'sonner';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
+import { ExternalToast, toast } from 'sonner';
 
 import { About as AboutData } from '../../@types';
 import {
@@ -9,14 +9,21 @@ import {
   Icon,
   Input,
   Sidebar,
+  Spinner,
   Text,
 } from '../../components';
 import { ACCEPT_EXTENSION_FILES } from '../../config';
+import { useUser } from '../../hooks';
 
 export function About() {
+  const { isLoaded, about: aboutFirebase } = useUser();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [about, setAbout] = useState<AboutData>({} as AboutData);
 
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const toastOptions: ExternalToast = { duration: 7500 };
 
   function handleOpenExplorer(): void {
     if (inputRef.current) inputRef.current.click();
@@ -32,7 +39,7 @@ export function About() {
       if (!isAcceptFile) {
         toast.error(
           'Formato da imagem selecionada é inválido! Os formatos aceitos são: PNG, JPG ou JPEG.',
-          { duration: 7500 },
+          toastOptions,
         );
         return;
       }
@@ -50,11 +57,19 @@ export function About() {
     if (!about?.uri) {
       toast.error(
         'Cadastro Incompleto! Você precisa enviar uma foto de perfil para finalizar o processo.',
-        { duration: 7500 },
+        toastOptions,
       );
       return;
     }
+
+    setIsLoading(true);
+
+    setIsLoading(false);
   }
+
+  useEffect(() => {
+    if (aboutFirebase?.id) setAbout(aboutFirebase);
+  }, [aboutFirebase]);
 
   return (
     <div className='flex w-full h-screen'>
@@ -73,103 +88,126 @@ export function About() {
           </Text>
         </header>
 
-        <form
-          onSubmit={handleSubmitAbout}
-          className='flex flex-col mx-auto lg:min-w-[40rem]'
-        >
-          <div className='flex flex-col lg:flex-row justify-center items-center lg:items-start gap-4'>
-            <div className='flex flex-col justify-center items-center gap-2'>
-              <Text type='medium' color='primary' toCenter>
-                Foto de Perfil
-              </Text>
+        {isLoaded ? (
+          <form
+            onSubmit={handleSubmitAbout}
+            className='flex flex-col mx-auto lg:min-w-[40rem]'
+          >
+            <div className='flex flex-col lg:flex-row justify-center items-center lg:items-start gap-4'>
+              <div className='flex flex-col justify-center items-center gap-2'>
+                <Text type='medium' color='primary' toCenter>
+                  Foto de Perfil
+                </Text>
 
-              <button
-                type='button'
-                className='flex size-44 justify-center items-center rounded-lg ring-2 ring-primary cursor-pointer hover:opacity-90 transition-colors duration-300 focus:outline-none focus:ring focus:ring-primary60 focus:border-primary60'
-                onClick={handleOpenExplorer}
-              >
-                <input
-                  type='file'
-                  ref={inputRef}
-                  accept='image/png, image/jpg, image/jpeg'
-                  className='hidden'
-                  multiple={false}
-                  readOnly
-                  onChange={handleGetFile}
-                />
+                <button
+                  type='button'
+                  className='flex size-44 justify-center items-center rounded-lg overflow-hidden ring-2 ring-primary cursor-pointer hover:opacity-90 transition-colors duration-300 focus:outline-none focus:ring focus:ring-primary60 focus:border-primary60'
+                  disabled={isLoading}
+                  onClick={handleOpenExplorer}
+                >
+                  <input
+                    type='file'
+                    ref={inputRef}
+                    accept='image/png, image/jpg, image/jpeg'
+                    className='hidden'
+                    multiple={false}
+                    readOnly
+                    onChange={handleGetFile}
+                  />
 
-                <DotButton variant='medium' type='add' mode='figure' />
-              </button>
-            </div>
+                  {about.uri ? (
+                    <img
+                      src={about.uri}
+                      alt={`Foto de ${about.name}`}
+                      className='w-full h-full object-cover'
+                    />
+                  ) : (
+                    <DotButton variant='medium' type='add' mode='figure' />
+                  )}
+                </button>
+              </div>
 
-            <Input
-              value={about.description || ''}
-              id='input-about'
-              variant='textarea'
-              mode='nude'
-              label='Sobre mim'
-              placeholder='Uma bibliografia do seu trabalho...'
-              maxLength={2096}
-              isRequired
-              onChange={description => setAbout({ ...about, description })}
-            />
-          </div>
-
-          <div className='hidden md:flex flex-col my-4 gap-1'>
-            <Text className='flex flex-row gap-1'>
-              <Icon variant='info' color='primary' /> Clique em{' '}
-              <Icon
-                variant='plus'
-                color='primary'
-                isCursorPointer
-                onClick={handleOpenExplorer}
-              />{' '}
-              para adicionar a foto de perfil.
-            </Text>
-
-            <Text className='flex flex-row gap-1'>
-              <Icon variant='info' color='primary' /> Para trocar de foto, basta
-              clicar na foto de perfil.
-            </Text>
-          </div>
-
-          <div className='flex flex-col gap-4'>
-            <Input
-              value={about.name || ''}
-              id='input-name'
-              variant='input'
-              type='text'
-              mode='nude'
-              label='Nome'
-              placeholder='Nome completo'
-              maxLength={256}
-              isRequired
-              onChange={name => setAbout({ ...about, name })}
-            />
-
-            <div className='flex flex-col lg:flex-row gap-4 justify-center items-end'>
               <Input
-                value={about.additional || ''}
-                id='input-additional'
+                value={about.description || ''}
+                id='input-about'
                 variant='textarea'
                 mode='nude'
-                label='Informações Adicionais'
-                placeholder='Fale mais sobre você...'
-                maxLength={512}
+                label='Sobre mim'
+                placeholder='Uma bibliografia do seu trabalho...'
+                maxLength={2096}
+                isDisabled={isLoading}
                 isRequired
-                onChange={additional => setAbout({ ...about, additional })}
-              />
-
-              <GenericButton
-                title='Salvar'
-                variant='primary'
-                type='medium'
-                mode='submit'
-                isHugWidth
+                onChange={description => setAbout({ ...about, description })}
               />
             </div>
+
+            <div className='hidden md:flex flex-col my-4 gap-1'>
+              <Text className='flex flex-row gap-1'>
+                <Icon variant='info' color='primary' /> Clique em{' '}
+                <Icon
+                  variant='plus'
+                  color='primary'
+                  isCursorPointer={!isLoading}
+                  onClick={handleOpenExplorer}
+                />{' '}
+                para adicionar a foto de perfil.
+              </Text>
+
+              <Text className='flex flex-row gap-1'>
+                <Icon variant='info' color='primary' /> Para trocar de foto,
+                basta clicar na foto de perfil.
+              </Text>
+            </div>
+
+            <div className='flex flex-col gap-4'>
+              <Input
+                value={about.name || ''}
+                id='input-name'
+                variant='input'
+                type='text'
+                mode='nude'
+                label='Nome'
+                placeholder='Nome completo'
+                maxLength={256}
+                isDisabled={isLoading}
+                isRequired
+                onChange={name => setAbout({ ...about, name })}
+              />
+
+              <div className='flex flex-col lg:flex-row gap-4 justify-center items-end'>
+                <Input
+                  value={about.additional || ''}
+                  id='input-additional'
+                  variant='textarea'
+                  mode='nude'
+                  label='Informações Adicionais'
+                  placeholder='Fale mais sobre você...'
+                  maxLength={512}
+                  isDisabled={isLoading}
+                  onChange={additional => setAbout({ ...about, additional })}
+                />
+
+                <GenericButton
+                  title='Salvar'
+                  variant='primary'
+                  type='medium'
+                  mode='submit'
+                  isDisabled={isLoading}
+                  isLoading={isLoading}
+                  isHugWidth
+                />
+              </div>
+            </div>
+          </form>
+        ) : (
+          <div className='flex flex-col w-full h-full gap-2 justify-center items-center'>
+            <Text color='primary' type='medium' toCenter>
+              Estamos carregando suas informações...
+            </Text>
+
+            <Spinner />
           </div>
-        </form>
+        )}
       </Container>
     </div>
   );
